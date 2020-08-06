8d804cc98988e027d0f14a6d41102520e35035cb
m-uppet <alex.pizigo@gmail.com>
2020-08-06 16:05:23 +0200

    add: gamma correction, shadow acne quick fix

--- pathtracer/color.h
+++ pathtracer/color.h
@@ -10,11 +10,11 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    // Divide by number of samples
+    // Divide by number of samples, and gamma correct for gamme = 2.0.
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt( r * scale);
+    g = sqrt( g * scale);
+    b = sqrt( b * scale);
 
     out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << " "
         << static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << " "
