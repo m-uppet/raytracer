89444d9f22a4fc6cca6507644b37e051781d1f69
m-uppet <alex.pizigo@gmail.com>
2020-08-27 16:15:50 +0200

    add: modified perlin noise to use grid of vectors instead of floats

--- pathtracer/perlin.h
+++ pathtracer/perlin.h
@@ -1,26 +1,12 @@
 #ifndef PERLIN_H
 #define PERLIN_H
 
-inline double trilinear_interp(double c[2][2][2], double u, double v, double w) {
-    auto accum = 0.0;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) {
-                accum += (i*u + (1-u)*(1-i))*
-                         (j*v + (1-v)*(1-j))*
-                         (k*w + (1-w)*(1-k))*c[i][j][k];
-            }
-        }
-    }
-    return accum;
-}
-
 class perlin {
 public:
     perlin() {
-        random_float = new double[point_count];
+        random_vectors = new vec3[point_count];
         for (int i = 0; i < point_count; ++i) {
-            random_float[i] = random_double();
+            random_vectors[i] = unit_vector(vec3::random(-1, 1));
         }
 
         perm_x = perlin_generate_perm();
@@ -29,7 +15,7 @@ public:
     }
 
     ~perlin() {
-        delete[] random_float;
+        delete[] random_vectors;
         delete[] perm_x;
         delete[] perm_y;
         delete[] perm_z;
@@ -40,21 +26,15 @@ public:
         auto v = p.y() - floor(p.y());
         auto w = p.z() - floor(p.z());
 
-        // Hermitian smoothing
-        u = u*u*(3-2*u);
-        v = v*v*(3-2*v);
-        w = w*w*(3-2*w);
-
-
-        int i = floor(p.x());
-        int j = floor(p.y());
-        int k = floor(p.z());
-        double c[2][2][2];
+        auto i = static_cast<int>(floor(p.x()));
+        auto j = static_cast<int>(floor(p.y()));
+        auto k = static_cast<int>(floor(p.z()));
+        vec3 c[2][2][2];
 
         for (int di = 0; di < 2; di++) {
             for (int dj = 0; dj < 2; dj++) {
                 for (int dk = 0; dk < 2; dk++) {
-                    c[di][dj][dk] = random_float[
+                    c[di][dj][dk] = random_vectors[
                                         perm_x[(i+di) & 255] ^
                                         perm_y[(j+dj) & 255] ^
                                         perm_z[(k+dk) & 255]
@@ -63,12 +43,12 @@ public:
             }
         }
 
-        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
     }
 
 private:
     static const int point_count = 256;
-    double* random_float;
+    vec3* random_vectors;
     int* perm_x;
     int* perm_y;
     int* perm_z;
@@ -93,6 +73,26 @@ private:
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    inline double perlin_interp(vec3 c[2][2][2], double u, double v, double w) const {
+        // Hermitian smoothing
+        auto uu = u*u*(3-2*u);
+        auto vv = v*v*(3-2*v);
+        auto ww = w*w*(3-2*w);
+        auto accum = 0.0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    vec3 weight_v(u-i, v-j, w-k);
+                    accum += (i*uu + (1-i)*(1-uu))*
+                             (j*vv + (1-j)*(1-vv))*
+                             (k*ww + (1-k)*(1-ww))*dot(c[i][j][k], weight_v);
+                }
+            }
+        }
+        return accum;
     }
 };
 
--- pathtracer/texture.h
+++ pathtracer/texture.h
@@ -51,7 +51,9 @@ public:
     noise_texture(double sc) : scale(sc) {}
 
     virtual color value(double u, double v, const point3& p) const override {
-        return color(1, 1, 1) * noise.noise(scale * p);
+        // Make sure the return value is between 0 and 1 to avoid NaNs in color.h sqrt() in the
+        // gamma function
+        return color(1, 1, 1) * 0.5 * (1.0 + noise.noise(scale * p));
     }
 
 public:
